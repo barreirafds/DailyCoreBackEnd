a4e54d39a64a9889361b3e46862fd9b749a69810
Diego Barreira <contact.diegobarreira@gmail.com>
2026-03-24 14:36:44 +0100

    Entity Test Implemented - Connection between the database and the backend done

---
+++ src/main/java/be/dailycorebackend/dal/entity/TestEntity.java
@@ -0,0 +1,31 @@
+package be.dailycorebackend.dal.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class TestEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public TestEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
