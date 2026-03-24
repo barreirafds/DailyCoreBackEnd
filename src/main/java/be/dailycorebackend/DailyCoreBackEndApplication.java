11c817b6b0593eef5a3fb4823cfcd6e17da6c049
Diego Barreira <contact.diegobarreira@gmail.com>
2026-03-24 14:12:10 +0100

    Initial commit

---
+++ src/main/java/be/dailycorebackend/DailyCoreBackEndApplication.java
@@ -0,0 +1,13 @@
+package be.dailycorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyCoreBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyCoreBackEndApplication.class, args);
+    }
+
+}
