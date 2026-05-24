23668a3564952118b9b430e86d2d95b65adc1093
Diego Barreira <contact.diegobarreira@gmail.com>
2026-05-25 00:51:09 +0100

    controllers updated

--- src/main/java/be/dailycorebackend/api/controller/RoutineController.java
+++ src/main/java/be/dailycorebackend/api/controller/RoutineController.java
@@ -1,8 +1,11 @@
 package be.dailycorebackend.api.controller;
 
 import be.dailycorebackend.api.dto.CreateRoutineRequest;
+import be.dailycorebackend.api.dto.RoutineResponse;
+import be.dailycorebackend.api.dto.UpdateRoutineRequest;
 import be.dailycorebackend.bll.service.RoutineService;
-import be.dailycorebackend.dal.entity.Routine;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,13 +20,30 @@ public class RoutineController {
         this.routineService = routineService;
     }
 
+    @GetMapping
+    public List<RoutineResponse> getAllRoutines() {
+        return routineService.getAllRoutines();
+    }
+
+    @GetMapping("/{id}")
+    public RoutineResponse getRoutineById(@PathVariable Long id) {
+        return routineService.getRoutineById(id);
+    }
+
     @PostMapping
-    public Routine createRoutine(@RequestBody CreateRoutineRequest request) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public RoutineResponse createRoutine(@Valid @RequestBody CreateRoutineRequest request) {
         return routineService.createRoutine(request);
     }
 
-    @GetMapping
-    public List<Routine> getAllRoutines() {
-        return routineService.getAllRoutines();
+    @PutMapping("/{id}")
+    public RoutineResponse updateRoutine(@PathVariable Long id, @Valid @RequestBody UpdateRoutineRequest request) {
+        return routineService.updateRoutine(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteRoutine(@PathVariable Long id) {
+        routineService.deleteRoutine(id);
     }
 }
--- src/main/java/be/dailycorebackend/api/controller/TaskController.java
+++ src/main/java/be/dailycorebackend/api/controller/TaskController.java
@@ -1,14 +1,15 @@
 package be.dailycorebackend.api.controller;
 
+import be.dailycorebackend.api.dto.CompleteTaskRequest;
 import be.dailycorebackend.api.dto.CreateTaskRequest;
+import be.dailycorebackend.api.dto.TaskResponse;
+import be.dailycorebackend.api.dto.UpdateTaskRequest;
 import be.dailycorebackend.bll.service.TaskService;
-import be.dailycorebackend.dal.entity.Task;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/api/tasks")
 public class TaskController {
 
     private final TaskService taskService;
@@ -17,13 +18,25 @@ public class TaskController {
         this.taskService = taskService;
     }
 
-    @PostMapping
-    public Task createTask(@RequestBody CreateTaskRequest request) {
-        return taskService.createTask(request);
+    @PostMapping("/api/routines/{routineId}/tasks")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TaskResponse createTask(@PathVariable Long routineId, @Valid @RequestBody CreateTaskRequest request) {
+        return taskService.createTask(routineId, request);
     }
 
-    @GetMapping
-    public List<Task> getAllTasks() {
-        return taskService.getAllTasks();
+    @PutMapping("/api/tasks/{taskId}")
+    public TaskResponse updateTask(@PathVariable Long taskId, @Valid @RequestBody UpdateTaskRequest request) {
+        return taskService.updateTask(taskId, request);
+    }
+
+    @PatchMapping("/api/tasks/{taskId}/complete")
+    public TaskResponse updateTaskCompleted(@PathVariable Long taskId, @RequestBody CompleteTaskRequest request) {
+        return taskService.updateTaskCompleted(taskId, request);
+    }
+
+    @DeleteMapping("/api/tasks/{taskId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTask(@PathVariable Long taskId) {
+        taskService.deleteTask(taskId);
     }
 }
